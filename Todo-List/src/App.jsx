ab3f7fe5e432c70b069ec8c155a1c24801bf928f
Kylee <Kykybrown2002@gmail.com>
2023-10-08 16:04:58 -0500

    last edits from dday 1

--- Todo-List/src/App.jsx
+++ Todo-List/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
+import Dashboard from './components/Dahboard'
 
 function App() {
   return (
+    <>
     <Header navClass='nav' title='ToDo List App'/>
-  )
+    <Dashboard className='dash'/>
+    </>
+  );
 }
 
 export default App
---
+++ Todo-List/src/components/Dahboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = (props) => {
+    return (
+        <div className={props.className}></div>
+    )
+}
+
+export default Dashboard
