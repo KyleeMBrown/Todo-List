1280ca0177e06c1537231aba096b7206326b9f7e
Kylee <Kykybrown2002@gmail.com>
2023-10-09 12:06:08 -0500

    cleaned up button styling

--- Todo-List/src/components/TodoList.jsx
+++ Todo-List/src/components/TodoList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState} from "react";
+import {useState} from "react";
 
 const TodoList = (props) => {
     const[todoList, setTodoList]= useState([])
@@ -34,3 +34,5 @@ const TodoList = (props) => {
 }
 
 export default TodoList
+
+
